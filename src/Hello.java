c1bc30a75e88c80471975dd4a152045d5b8c8671
kjt1024 <108518926+kjt1024@users.noreply.github.com>
2022-07-02 18:24:30 +0800

    Update Hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
         System.out.println(2);
         System.out.println(3);
         System.out.println("work");
-        System.out.println("work2");
+        System.out.println("work2hhhhhhh");
     }
 }
